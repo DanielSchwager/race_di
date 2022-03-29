3a45255be3996ee0ee37a0887ffa8794ecf284a5
DanielSchwager <danieleric.schwager@stud.hslu.ch>
2022-03-29 18:01:38 +0200

    ball physics and controller
    29.3

---
+++ di_race_project/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthbar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealt(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthbar.SetHealth(currentHealth);
+    }
+}
---
+++ install_README.txt
@@ -0,0 +1,7 @@
+1. Window/Package Manager
+Input System -> install
+
+
+
+2. Edit/Project Settings
+Configuration Active Handling ->Both
